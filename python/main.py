dd7ca420e2b1d988f6aead11c7542bd8fc220219
Praveen Venkatesh <pveenvenkatesh@gmail.com>
2019-11-06 20:08:27 +0530

    sdf

--- python/main.py
+++ python/main.py
@@ -18,36 +18,36 @@ def on_message(ws, message):
     autopy.mouse.move(width,height)
     # pyautogui.moveRel(int(float(x[0])*10), int(float(x[1])*10))
     
-#TEST
-i = 0 
+# #TEST
+# i = 0 
           
-sleep(1)
-autopy.mouse.move(0,0)
-while(True):
+# sleep(1)
+# autopy.mouse.move(0,0)
+# while(True):
     
-    autopy.mouse.move(i,i)
-    sleep(10/1000)   
-    i = i + 1
+#     autopy.mouse.move(i,i)
+#     sleep(10/1000)   
+#     i = i + 1
 
-# def on_error(ws, error):
-#     print (error)
+def on_error(ws, error):
+    print (error)
 
-# def on_close(ws):
-#     print ("### closed ###")
+def on_close(ws):
+    print ("### closed ###")
 
-# def on_open(ws):
-#     def run(*args):
-#         for i in range(3):
-#             time.sleep(1)
+def on_open(ws):
+    def run(*args):
+        for i in range(3):
+            time.sleep(1)
             
-#         time.sleep(1)
-#         ws.close()
-#         print ("thread terminating...")
-#     thread.start_new_thread(run, ())
+        time.sleep(1)
+        ws.close()
+        print ("thread terminating...")
+    thread.start_new_thread(run, ())
 
-# ws = websocket.WebSocketApp("ws://192.168.43.209:81",
-#                               on_message = on_message,
-#                               on_error = on_error,
-#                               on_close = on_close)
-# ws.on_open = on_open
-# ws.run_forever()                    
+ws = websocket.WebSocketApp("ws://192.168.43.209:81",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+ws.on_open = on_open
+ws.run_forever()                    
